6cdb4cee33ee2a6cd7ff387c993e272693d7e83f
Emmanuel <emmanuelukwe1@gmail.com>
2021-09-21 13:59:10 -0700

    make small edit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">    
     <meta name="viewport" content="width=device-width, initial_scale=0.1">
-    <title>compress</title>
+    <title>Mr.Pinnacle Compressor</title>
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="./bootstrap/js/bootstrap.js">
     <link rel="stylesheet" href="style.css">
